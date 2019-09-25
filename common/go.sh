4d0ad5b9b432200774bf43a786be56369cb95a51
Gerald Quimpo <gerald@catalyst.net.nz>
2019-09-25 15:21:51 +1200

    helper scripts for running individual features.

---
+++ common/go.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+  echo 'Please specify feature file (.../features/(.*\.feature))'
+  exit -1
+fi
+
+cd /var/www/html/mahara
+./test/behat/mahara_behat.sh runheadless $1 html
