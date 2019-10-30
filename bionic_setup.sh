e791eb16f47b52fef0042f288defd4cdb1db0a0c
Gerald Quimpo <gerald@catalyst.net.nz>
2019-10-30 13:17:02 +1300

    initial bionic scripts

---
+++ bionic_setup.sh
@@ -0,0 +1,129 @@
+#!/bin/bash
+
+# if you're gerald, do tiger.sh first.
+
+# if you're in NZ, this is faster.  uncommented in tiger.sh.  
+#cat /etc/apt/sources.list| sed "s/\/\/archive.ubuntu.com/nz.archive.ubuntu.com/" > /tmp/sources.list
+#sudo mv /etc/apt/sources.list /etc/apt/sources.list.ori
+#sudo mv /tmp/sources.list /etc/apt/sources.list
+#sudo apt-get update
+
+# also uncommented in tiger.sh
+#sudo apt install -y tightvncserver icewm
+#sudo apt-get install -y openssh-server
+
+# install required packages for Ubuntu 18.04 (Wiki: Dev Area/Dev Env)
+#sudo apt-get install -y apache2 make curl wget xvfb git gitk postgresql php-cli libapache2-mod-php php-curl php-gd php-json php-ldap php-pgsql php-xmlrpc php-zip php-xml php-mbstring nodejs-legacy npm  firefox
+
+sudo apt-get -y install make curl wget xvfb git gitk postgresql
+sudo apt-get -y install php7.2-common php-cli libapache2-mod-php7.2 php7.2-curl php7.2-gd
+sudo apt-get -y install php-json php-ldap php-pgsql php-xmlrpc php-zip php-xml php-mbstring 
+
+sudo apt-get -y install libssl-dev
+
+
+a2enmod php7.2
+
+#ff might not be necessary if already disabled
+a2dismod mpm_worker
+a2dismod mpm_event
+
+echo "Now installing postfix, please select Local only"
+sudo apt-get install -y postfix
+
+# is memcached required?  it's not in the docs?  -- still the errors though.  this isn't the reason.
+sudo apt-get install -y memcached
+
+# install npm from here even though bionic has 8.x
+# there is a version dependency issue with node-gyp as of 2019-10-30
+## from: https://tecadmin.net/install-latest-nodejs-npm-on-ubuntu/
+## install newer versions of node.  the one that comes with xenial is 4.x? (to be confirmed)
+curl -sSL https://deb.nodesource.com/gpgkey/nodesource.gpg.key | sudo apt-key add -
+VERSION=node_8.x
+DISTRO="$(lsb_release -s -c)"
+echo "deb https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee /etc/apt/sources.list.d/nodesource.list
+echo "deb-src https://deb.nodesource.com/$VERSION $DISTRO main" | sudo tee -a /etc/apt/sources.list.d/nodesource.list
+
+sudo apt-get update
+sudo apt-get install -y nodejs npm
+
+# setup mahara under /var/www/html/mahara
+# we prefer this to ~/code because we don't (particularly on actual dev machines, not lxc or VMs)
+# to have ~ be g+rwx or even g+rx.  files are $USER:www-data so make css' chmods work (EPERM if
+# we have www-data:$USER
+
+CODE=/var/www/html/mahara
+sudo mkdir $CODE
+sudo chown $USER:www-data $CODE
+sudo chmod -R g+rwx $CODE
+pushd $CODE/../
+
+git clone https://git.mahara.org/mahara/mahara.git
+
+popd
+
+sudo chown -R $USER:www-data $CODE
+sudo chmod -R ug=rwx $CODE
+
+# password optional if you're going to use some other auth method.
+sudo -u postgres createuser -D -R -S maharauser
+sudo -u postgres psql -c "alter user maharauser with password 'maharapassword'"
+
+sudo -u postgres createdb -Omaharauser mahara-master
+
+# edit $CODE/htdocs/config.php to have your actual email address.
+cat common/config.php | sed "s/EMAIL_ADDRESS/$USER@example.com/" > $CODE/htdocs/config.php
+
+sudo mkdir /var/lib/maharadata
+sudo mkdir /var/lib/maharadata/master
+sudo chown www-data:www-data /var/lib/maharadata/master
+
+sudo sh -c "echo '127.0.0.1 mahara' >> /etc/hosts"
+
+# WARNING: do this manually if your current post_max_size is larger than 32M.
+for fn in `find /etc -name php.ini`
+do
+  sudo sh -c "echo 'post_max_size = 32M' >> $fn"
+done
+
+sudo cp common/apache-mahara.conf /etc/apache2/sites-available/mahara.conf
+sudo a2ensite mahara
+sudo apache2ctl configtest
+sudo apache2ctl graceful
+
+sudo sh -c "echo '* * * * * www-data /usr/bin/php /home/<your username>/code/mahara/htdocs/lib/cron.php >/dev/null 2>&1' > /etc/cron.d/mahara"
+
+pushd $CODE
+npm cache clean --force
+npm install
+sudo npm install -g gulp
+make css
+popd
+
+## BEHAT:
+
+sudo apt-get install -y curl openjdk-8-jre-headless chromium-browser
+
+sudo mkdir -p /var/lib/maharadata/master_behat
+sudo chown -R $USER:www-data /var/lib/maharadata/master_behat
+sudo chmod -R ug=rwx /var/lib/maharadata/master_behat
+
+cp common/gg.feature /var/www/html/mahara/test/behat/features/security
+
+cd /var/www/html/mahara/
+
+sudo mkdir -p /var/lib/maharadata/master_behat
+sudo chmod -R ug+rwx /var/lib/maharadata/master_behat
+sudo chown -R $USER:www-data /var/lib/maharadata
+
+make css
+make initcomposer
+
+# we could do the composer.phar and cli init manually too
+# but this is simple.
+./test/behat/mahara_behat.sh run security/gg.feature html
+./test/behat/mahara_behat.sh run security/gg.feature html
+
+
+
+
---
+++ init-bionic.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+if [ -z $1 ]
+then
+  echo "Missing lxc instance name.  Usage: $0 [name]"
+  exit -1
+fi
+
+export NAME=$1
+
+sudo lxc-create -n $NAME -t download -- -d ubuntu -a amd64 -r bionic
+sudo lxc-start -n $NAME
+sleep 5
+sudo lxc-attach -n $NAME -- bash -c "cd /root;apt install -y git;git clone https://github.com/bopolissimus/init-xenial-mahara.git"
+
+sudo lxc-attach -n $NAME -- bash -c "cd /root;cd init-xenial-mahara;./tiger.sh"
+
+# so we don't have permissions issues when we do pushd and popd as -i -u gerald
+sudo lxc-attach -n $NAME -- bash -c "cd /home/gerald;cp -Rf /root/init-xenial-mahara .;chown -R gerald:gerald init-xenial-mahara"
+
+#cd /home/gerald
+#sudo cp -Rf /root/init-xenial-mahara .
+#chown -R gerald:gerald init-xenial-mahara
+
+# run as gerald (with -i so $HOME is also set correctly) so that
+# files are owned by gerald, not root
+# during the postfix dialog, select local only.
+sudo lxc-attach -n $NAME -- bash -c "/usr/bin/sudo -i -u gerald /bin/bash -c 'cd init-xenial-mahara;./bionic_setup.sh'"
+
+
